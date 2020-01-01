d83bf7515ba4a48cf407a778b9cfdc56203a3d98
Roman Galeev <jamhedd@gmail.com>
2020-01-01 18:41:01 +0100

    Fix development setting

--- ui/public/config.js
+++ ui/public/config.js
@@ -1,5 +1,5 @@
 window.argovue = {}
-window.argovue.api_base_url = "http://localhost:8080"
+window.argovue.api_base_url = ""
 window.argovue.version = '_VERSION_'
 window.argovue.commit = '_COMMIT_'
 window.argovue.builddate = '_BUILDDATE_'
