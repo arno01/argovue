f490153e6170109cde133861ab75c0a20c0eedfa
Roman Galeev <jamhedd@gmail.com>
2020-01-02 17:06:03 +0100

    Bump chart release to v0.2.13

--- bump.sh
+++ bump.sh
@@ -4,8 +4,15 @@ MINOR=$(echo $TAG | sed -E 's|^.*\.([0-9]+)$|\1|')
 MAJOR=$(echo $TAG | sed -E 's|^v(.*)\.[0-9]+$|\1|')
 NEWVER="$MAJOR.$((MINOR+1))"
 git tag v$NEWVER
+
+CHART=$(yq r helm/argovue/Chart.yaml version)
+CHARTMINOR=$(echo $TAG | sed -E 's|^.*\.([0-9]+)$|\1|')
+CHARTMAJOR=$(echo $TAG | sed -E 's|(.*)\.[0-9]+$|\1|')
+NEWCHART="$CHARTMAJOR.$((CHARTMINOR+1))"
 yq w -i helm/argovue/Chart.yaml appVersion $NEWVER
-git commit helm/argovue/Chart.yaml -m "Bump chart version to v$NEWVER"
+yq w -i helm/argovue/Chart.yaml version $NEWCHART
+git commit helm/argovue/Chart.yaml -m "Bump chart version to app:v$NEWVER chart:$NEWCHART"
 make helm
 git commit docs -m "Bump chart release to v$NEWVER"
+git push --tags
 git push
