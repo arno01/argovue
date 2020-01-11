6aeb76cedbfa3f8d19f31150fe2086924a324ddd
Roman Galeev <jamhedd@gmail.com>
2020-01-11 17:48:37 +0100

    Fix bump

--- bump.sh
+++ bump.sh
@@ -4,4 +4,4 @@ MINOR=$(echo $TAG | sed -E 's|^.*\.([0-9]+)$|\1|')
 MAJOR=$(echo $TAG | sed -E 's|^v(.*)\.[0-9]+$|\1|')
 NEWVER="$MAJOR.$((MINOR+1))"
 git tag v$NEWVER
-git push v$NEWVER
+git push origin v$NEWVER
